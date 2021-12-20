f28a7df574e211526561c84c146d22a0ab056f08
ViralSir <viralsir2018@gmail.com>
2021-12-20 10:11:50 +0530

    login and logout functionality added.

--- saket_django/urls.py
+++ saket_django/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from users.views import home
+from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',home,name="home")
+    path('home/',home,name="home"),
+    path("login/",LoginView.as_view(template_name="users/sign-in.html"),name="login"),
+    path("logout/",LogoutView.as_view(template_name="users/sign-out.html"),name="logout")
+
 ]
