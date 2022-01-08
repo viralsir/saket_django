9068ae80310a5adc0f27374f751c30b755071b61
ViralSir <viralsir2018@gmail.com>
2022-01-08 17:43:21 +0530

    course detail and deleteview added.

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,9 @@
 from django.urls import  path
-from .views import NewCourseView,ListCourseView,UpdateCourseView
+from .views import NewCourseView,ListCourseView,UpdateCourseView,DetailCourseView,DeleteCourseView
 urlpatterns=[
      path("new/",NewCourseView.as_view(),name="new-course"),
      path("view/",ListCourseView.as_view(),name="list-course"),
-     path("update/<int:pk>",UpdateCourseView.as_view(),name="update-course")
+     path("update/<int:pk>",UpdateCourseView.as_view(),name="update-course"),
+     path("detail/<int:pk>",DetailCourseView.as_view(),name="detail-course"),
+     path("delete/<int:pk>",DeleteCourseView.as_view(),name="delete-course")
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView,UpdateView,DeleteView,ListView
+from django.views.generic import CreateView,UpdateView,DeleteView,ListView,DetailView
 from .models import course
 
 # Create your views here.
@@ -21,3 +21,13 @@ class UpdateCourseView(UpdateView):
     fields = '__all__'
 
 #course_form.html
+class DetailCourseView(DetailView):
+    model = course
+
+#course_detail.html
+class DeleteCourseView(DeleteView):
+    model = course
+    template_name = "courses/course_delete_confirm.html"
+    success_url = '/course/view'
+
+#course_delete_confirm.html
